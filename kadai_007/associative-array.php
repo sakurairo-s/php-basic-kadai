519a5ba2efe621535ecfffa506fc94848ef304cf
sakurairo-s <s_shibazaki_eng@outlook.jp>
2025-04-14 23:28:35 +0900

    フォルダをアップロード
    課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>課題_連想配列を作ってキーと値を出力する</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    //連想配列に値を代入する
+    $items = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    //連想配列の値を出力する
+    print_r($items);
+    ?>
+  </p>
+</body>
+
+</html>
