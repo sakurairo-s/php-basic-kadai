960a5cf97c7232bc5386764cf61b4acd504afc67
sakurairo-s <s_shibazaki_eng@outlook.jp>
2025-04-18 23:12:53 +0900

    フォルダをアップロード
    課題提出

---
+++ kadai_sort/sort.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+
+        //ソート関数の定義
+        function sort_2way($array,$order) {
+        if ($order === true) {
+        echo '昇順でソートします。<br>';
+        sort($array);
+        } else {
+        echo '降順でソートします。<br>';
+        rsort($array);
+        }
+
+        foreach ($array as $num) {
+          echo "$num<br>"; 
+        }
+
+      }
+
+        //配列の定義
+        $nums = [15, 4, 18, 23, 10];
+
+        //ソート関数の呼び出し
+        sort_2way($nums, TRUE);
+        sort_2way($nums, FALSE);
+        ?>
+    </p>
+</body>
+
+</html>
